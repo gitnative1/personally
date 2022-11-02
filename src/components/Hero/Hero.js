37f10660620c6f4ddcd79e37c4751f9ba1c5d1a4
asigitj <asigitj1@gmail.com>
2022-11-02 22:50:10 +0700

    edit hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -12,7 +12,7 @@ const Hero = () => (
         My Personal Portfolio
       </SectionTitle>
       <SectionText>
-      The purpose of software developer is to help Start-Up and established Companies to take their business to the next level and build awesome apps.
+      The purpose of a software developer is to help Start-Up and established Companies to take their business to the next level and build awesome apps.
       </SectionText>
       <Button onClick={() => window.location ="https://www.instagram.com/gitnative/" }>Get In Touch</Button>
     </LeftSection>
